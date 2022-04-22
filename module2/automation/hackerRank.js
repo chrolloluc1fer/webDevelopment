4df2421d401047373348c803d40e63dcbbd4a657
Shubham Gupta <mrjanuaryx@gmail.com>
2022-04-22 16:33:54 +0530

    hackerRank completed

--- module2/automation/hackerRank.js
+++ module2/automation/hackerRank.js
@@ -2,119 +2,142 @@ const puppeteer = require("puppeteer");
 const mail = "cetiti7811@carsik.com";
 const password = "chrollolucifer";
 const code = require('./code');
-let browserPromise = puppeteer.launch({ headless: false, defaultViewport: null, args: ['--start-fullscreen'] })
 
+let browserPromise = puppeteer.launch({ headless: false, defaultViewport: null,args: ['--start-fullscreen'] });
 let page;
-
-browserPromise.then(function (browser) {
-
+browserPromise.then(function(browser){
+    console.log("Browser is opened");
     let pagePromise = browser.newPage();
-    console.log("brower open hogya")
     return pagePromise;
-}).then(function (pageInstance) {
-    page = pageInstance;
-    let urlpromise = page.goto("https://www.hackerrank.com/");
-    console.log("link Open hogya")
-    return urlpromise;
-}).then(function () {
-    return waitAndClick("ul.menu a");
-}).then(function () {
+}).then(function(pageInstance){
+    console.log("Page is opened");
+    page = pageInstance
+    let urlPromise = page.goto('https://www.hackerrank.com/');
+    return urlPromise;
+}).then(function(){
+    return waitAndClick('ul.menu a');
+}).then(function(){
     let waitPromise = page.waitForSelector(".fl-module-content.fl-node-content .fl-button");
     return waitPromise;
-}).then(function () {
-    let domClickPromise = page.evaluate(function () {
+}).then(function(){
+    let domClickPromse = page.evaluate(function(){
         let btns = document.querySelectorAll(".fl-module-content.fl-node-content .fl-button");
         btns[1].click();
         return;
     });
-    return domClickPromise;
-}).then(function () {
+    return domClickPromse;
+}).then(function(){
     let waitPromise = page.waitForSelector("#input-1");
     return waitPromise;
-}).then(function () {
-    let mailTypePromise = page.type("#input-1", mail, { delay: 100 });
-    return mailTypePromise;
-}).then(function () {
-    let passwordTypePromise = page.type("#input-2", password, { delay: 100 });
-    return passwordTypePromise;
-}).then(function () {
-    let loginClickpromise = page.click("button[data-analytics='LoginPassword']");
-    return loginClickpromise;
-}).then(function () {
-    return waitAndClick("[Data-automation='algorithms']");
-}).then(function () {
-    return waitAndClick("[value=warmup]");
-}).then(function () {
-    let linkPromise = page.evaluate(function () {
+}).then(function(){
+    let mailTypedPromise = page.type("#input-1",mail,{ delay: 100 });
+    return mailTypedPromise;
+}).then(function(){
+    let passTypedPromise = page.type("#input-2",password,{delay:100});
+    return passTypedPromise;
+}).then(function(){
+    let clickPromse = page.click('button[data-analytics="LoginPassword"]');
+    return clickPromse;
+}).then(function(){
+    return waitAndClick('[data-automation="algorithms"]');
+})
+.then(function(){
+    return page.waitForSelector(".filter-group");
+}).then(function(){
+    let domSelectPromise = page.evaluate(function(){
+        let allDivs = document.querySelectorAll(".filter-group");
+        let div = allDivs[3];
+        let clickSelector = div.querySelector(".ui-checklist-list-item input");
+        clickSelector.click();
+        return;
+    })
+    return domSelectPromise;
+}).then(function(){
+    console.log("warmup Selected");
+    return page.waitForSelector('.challenges-list .js-track-click.challenge-list-item');
+}).then(function(){
+    let arrPromise = page.evaluate(function(){
         let arr = [];
-        let allChallenge = document.querySelectorAll(".js-track-click.challenge-list-item");
-        for (let i = 1; i < allChallenge.length; i++) {
-            arr.push(allChallenge[i].href);
+        let aTags = document.querySelectorAll('.challenges-list .js-track-click.challenge-list-item');
+        for(let i=0;i<aTags.length;i++){
+            let link =  aTags[i].href;
+            console.log(link);
+            arr.push(link);
         }
         return arr;
     })
-    return linkPromise;
-}).then(function (questionArr) {
-    let questionPromise = questionSolver(questionArr[0], code.answers[0])
+    return arrPromise;
+}).then(function(questionsArr){
+    console.log(questionsArr);
+    console.log(code.answers.length);
+    let questionPromise = questionSolver(questionsArr[0],code.answers[0]);
+    for(let i=1;i<questionsArr.length;i++){
+        questionPromise = questionPromise.then(function(){
+            let nextQuestionPromise = questionSolver(questionsArr[i],code.answers[i]);
+            return nextQuestionPromise;
+        })
+    }
     return questionPromise;
+}).then(function(){
+    console.log("All the warm up questions have been submitted!!!");
 })
 
 
 
-
-
-function waitAndClick(selector) {
-    return new Promise(function (resolve, reject) {
+function waitAndClick(selector){
+    return new Promise(function(resolve,reject){
         let waitPromise = page.waitForSelector(selector);
-        waitPromise.then(function () {
+        waitPromise.then(function(){
             let clickPromise = page.click(selector);
             return clickPromise;
-        }).then(function () {
-            resolve()
-        })
-
+        }).then(function(){
+            resolve();
+        });
     })
 }
 
 
-function questionSolver(link, solution) {
-    new Promise(function (resolve, reject) {
-        let linkPromise = page.goto(link);
-        linkPromise.then(function () {
-            return waitAndClick(".checkBoxWrapper input")
-        }).then(function () {
-            return waitAndClick("#input-1");
-        }).then(function () {
-            let typePromise = page.type("#input-1", solution);
+function questionSolver(question,answer){
+    return new Promise(function(resolve,reject){
+        let linkPromise = page.goto(question);
+        linkPromise.then(function(){
+            return waitAndClick('.checkBoxWrapper input');
+        }).then(function(){
+            return waitAndClick('.ui-tooltip-wrapper textarea');
+        }).then(function(){
+            let typePromise = page.type('.ui-tooltip-wrapper textarea',answer);
             return typePromise;
-        }).then(function () {
-            let holdControl = page.keyboard.down("Control")
+        }).then(function(){
+            let holdControl = page.keyboard.down('Control');
             return holdControl;
-        }).then(function () {
-            let holdA = page.keyboard.press("A");
-            return holdA;
-        }).then(function () {
-            let holdX = page.keyboard.press("X");
-            return holdX;
-        }).then(function () {
-            let leaveControl = page.keyboard.up("Control");
-            return leaveControl;
-        }).then(function () {
-            return waitAndClick(".monaco-editor.no-user-select.vs");
-        }).then(function () {
-            let holdControl = page.keyboard.down("Control")
-            return holdControl;
-        }).then(function () {
-            let holdA = page.keyboard.press("A");
-            return holdA;
-        }).then(function () {
-            let holdV = page.keyboard.press("V");
-            return holdV;
-        }).then(function () {
-            return waitAndClick(".ui-btn.ui-btn-normal.ui-btn-primary.pull-right.hr-monaco-submit.ui-btn-styled");
-        }).then(function () {
-            console.log("Success")
+        }).then(function(){
+            let pressA = page.keyboard.press('A');
+            return pressA;
+        }).then(function(){
+            let pressX = page.keyboard.press('X');
+            return pressX;
+        }).then(function(){
+            let upControl = page.keyboard.up('Control');
+            return upControl;
+        }).then(function(){
+            return waitAndClick('.monaco-editor.no-user-select.vs');
+        }).then(function(){
+            let holdControl = page.keyboard.down('Control');
+            return holdControl;  
+        }).then(function(){
+            let pressA = page.keyboard.press('A');
+            return pressA;
+        }).then(function(){
+            let pressV = page.keyboard.press('V');
+            return pressV;
+        }).then(function(){
+            let upControl = page.keyboard.up('Control');
+            return upControl;
+        }).then(function(){
+            return waitAndClick('.ui-btn.ui-btn-normal.ui-btn-primary.pull-right.hr-monaco-submit.ui-btn-styled');
+        }).then(function(){
+            console.log("questions submitted success");
+            resolve();
         })
     })
 }
-
