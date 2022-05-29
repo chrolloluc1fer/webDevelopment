3c3c54703d1a35251393078389e2f31d6b8e4269
Shubham Gupta <mrjanuaryx@gmail.com>
2022-05-29 15:55:15 +0530

    update

--- module3/excelClone/script.js
+++ module3/excelClone/script.js
@@ -4,6 +4,7 @@ let topLeftCell = document.querySelector(".top-left-cell")
 let allcell  = document.querySelectorAll(".cell")
 let address = document.querySelector("#address")
 
+
 cellsContentDiv.addEventListener("scroll",(e)=>{
     let scrollFromTop = e.target.scrollTop;
     let scrollFromLeft = e.target.scrollLeft;
@@ -13,11 +14,33 @@ cellsContentDiv.addEventListener("scroll",(e)=>{
     topLeftCell.style.left = scrollFromLeft+"px";
 })
  
+
 for(let i = 0; i < allcell.length;i++){
     allcell[i].addEventListener("click",(e)=>{
-        let rowid = Number(e.target.getAttribute("rowid"))
-        let colid = Number(e.target.getAttribute("colid"))
+        let id = getId(e.target);
+        let rowid = id.rowid;
+        let colid = id.colid;
         let add = String.fromCharCode(65+colid) + (rowid + 1)+"";
         address.value = add;
     })
+    allcell[i].addEventListener("blur",function(e){
+        let cellValue = e.target.textContent;
+        let id = getId(e.target)
+        let rowid = id.rowid;
+        let colid = id.colid;
+        let cellObj = db[rowid][colid];
+        cellObj.value = cellValue
+        
+        
+    })
 }
+
+
+
+function getId(element){
+    let rowid = Number(element.getAttribute("rowid"))
+    let colid= Number(element.getAttribute("colid"))
+    return {rowid,colid}
+}
+
+
