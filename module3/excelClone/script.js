40cf59cca1c2d6e777134b1b608f5d073fd62732
Shubham Gupta <mrjanuaryx@gmail.com>
2022-05-29 14:55:29 +0530

    update

--- module3/excelClone/script.js
+++ module3/excelClone/script.js
@@ -22,11 +22,28 @@ function init(){
         cellsContent+="<div class = 'row'>";
         //column
         for(let j=0;j<26;j++){
-            cellsContent+="<div class = 'cell' contentEditable></div>"
+            cellsContent+="<div class = 'cell' contentEditable>CELL</div>"
         }
         cellsContent+="</div>"
     }
     cellsContent+="</div>"
     cellsContentDiv.innerHTML = cellsContent;
 }
+
+
 init();
+
+
+let topRow = document.querySelector(".top-row")
+let leftCol = document.querySelector(".left-col")
+let topLeftCell = document.querySelector(".top-left-cell")
+
+cellsContentDiv.addEventListener("scroll",(e)=>{
+    let scrollFromTop = e.target.scrollTop;
+    let scrollFromLeft = e.target.scrollLeft;
+    topRow.style.top = scrollFromTop +"px";
+    leftCol.style.left = scrollFromLeft+"px";
+    topLeftCell.style.top = scrollFromTop+"px";
+    topLeftCell.style.left = scrollFromLeft+"px";
+
+})
