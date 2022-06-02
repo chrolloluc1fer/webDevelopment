aae28bdd01b3d460f8a818d0ab4a8fbc91e484dd
Shubham Gupta <mrjanuaryx@gmail.com>
2022-06-02 19:24:32 +0530

    update

---
+++ x.js
@@ -0,0 +1,37 @@
+setTimeout(function () {
+  console.log(1);
+});
+setTimeout(function () {
+  console.log(2);
+});
+
+
+
+setTimeout(() => {
+  let p = new Promise(function (resolve, reject) {
+    resolve();
+  });
+  p.then(function () {
+    console.log(4);
+  });
+  
+  p.then(function () {
+    console.log(5);
+  });
+  
+}, );
+
+setTimeout(() => {
+  console.log(3);
+},);
+
+
+
+setTimeout(function () {
+  console.log(6);
+});
+
+
+
+
+
