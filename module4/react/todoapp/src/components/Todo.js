177aac59fc38266311fde5249b00a1c509542f12
Shubham Gupta <mrjanuaryx@gmail.com>
2022-06-19 19:01:57 +0530

    todo completed

--- module4/react/todoapp/src/components/Todo.js
+++ module4/react/todoapp/src/components/Todo.js
@@ -4,21 +4,53 @@ class Todo extends  Component{
     constructor(){
         super();
         this.state ={
-            task:["Learn js","Learn DOM","learn React","Learn Node"]
+            tasks:[],
+            currentTask:""
         }
+      
+    }
+    handleChange = (e)=>{
+        this.setState({
+            currentTask:e.target.value
+        })
+    }
+
+    addTask = ()=>{
+        if(this.state.currentTask === "")
+        return;
+        this.setState({
+            tasks:[...this.state.tasks,{task:this.state.currentTask,id:this.state.tasks.length + 1}],
+            currentTask:""
+        })
+      
+       
+    }
+
+    removeTask = (id)=>{
+        let newArr = this.state.tasks.filter((taskObj)=>{
+            return taskObj.id != id;
+
+        })
+        this.setState({
+            tasks:[...newArr]
+        })
     }
     render(){
         return(
             <div>
-            <input></input>
-            <button>Add Task</button>
+            <input type="text" value={this.state.currentTask} onChange={this.handleChange}/>
+            <button onClick={this.addTask}>Add Task</button>
+          
             <ul>
-               {this.state.task.map((element)=>(
-                    <li>
-                        <p>{element}</p>
+               {this.state.tasks.map((taskObj)=>(
+                    <li key={taskObj.id}>
+                        <p>{taskObj.task}</p>
+                        <button onClick={() => this.removeTask(taskObj.id)}>DELETE</button>
                     </li>
                ))}
             </ul>
+    
+
             </div>
          
         )
