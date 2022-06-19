d3daa9a600a1881c6bb4d7c4c0674222c1327297
Shubham Gupta <mrjanuaryx@gmail.com>
2022-06-19 19:13:08 +0530

    movies app started

---
+++ module4/react/movies-app/src/component/NavBar.js
@@ -0,0 +1,19 @@
+import { Component } from "react";
+
+
+class NavBar extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+      return(
+            <div style={{display:"flex",padding:"1rem"}}>
+                <h1>Movies App</h1>
+                <h2 style={{marginLeft:"2rem",marginTop:"2rem"}}>Favourites</h2>
+            </div>
+      )  
+      
+    }
+}
+
+export default NavBar
