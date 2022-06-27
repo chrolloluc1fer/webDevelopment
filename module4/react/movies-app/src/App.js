54ff2f451d36020c0bc40d4fedf9eb342ddb0f52
Shubham Gupta <mrjanuaryx@gmail.com>
2022-06-27 19:17:48 +0530

    update

--- module4/react/movies-app/src/App.js
+++ module4/react/movies-app/src/App.js
@@ -2,16 +2,17 @@ import './App.css';
 import Banner from './components/Banner';
 import MovieList from './components/MovieList';
 import NavBar from './components/NavBar';
+import Fav from './components/Fav'
 
 function App() {
   return (
     <>
       <NavBar/>
-      <Banner/>
-      <MovieList/>
+      {/* <Banner/>
+      <MovieList/> */}
+      <Fav/>
     </>
     );
 }
-
 
 export default App;
--- module4/react/movies-app/src/components/MovieList.js
+++ module4/react/movies-app/src/components/MovieList.js
@@ -31,6 +31,26 @@ class MovieList extends Component {
             movies:[...data.results]
         })
     }
+    handleNext=()=>{
+        this.setState({ 
+            pArr:[...this.state.pArr,this.state.pArr.length+1],
+            currPage:this.state.currPage+1
+        },this.changeMovies)
+    }
+    handlePrev=()=>{
+        if(this.state.currPage!=1){
+            this.setState({
+                currPage:this.state.currPage-1
+            },this.changeMovies);
+        }
+    }
+    handlePageClick=(ele)=>{
+        if(ele!=this.state.currPage){
+            this.setState({
+                currPage: ele
+            },this.changeMovies);
+        }
+    }
 
     handleNext=()=>{
         this.setState({
@@ -40,7 +60,8 @@ class MovieList extends Component {
     }
 
     render() {
-      
+        console.log("rednered");
+        
         return (
             <>
                 <div>
@@ -48,11 +69,12 @@ class MovieList extends Component {
                 </div>
                 <div className="movies-list">
                     {this.state.movies.map((movieEle) => (
-                        <div className="card movie-card">
+                        <div className="card movie-card" onMouseEnter={()=>this.setState({hover:movieEle.id})} onMouseLeave={()=>this.setState({hover:""})} >
                             <img src={`https://image.tmdb.org/t/p/original${movieEle.backdrop_path}`} style={{ height: '40vh', width: '20vw' }} className="card-img-top movie-img" alt="..." />
                             <h5 className="card-title movie-title">{movieEle.title}</h5>
                             <div style={{ display: 'flex', justifyContent: "center" }}>
-                               <a href="#" type="button" className="btn btn-primary movies-button">Add to Favourites</a>
+                                {this.state.hover == movieEle.id && (
+                                <a href="#" type="button" className="btn btn-primary movies-button">Add to Favourites</a>)}
                             </div>
                         </div>
                     ))}
@@ -60,9 +82,9 @@ class MovieList extends Component {
                 <div style={{display:"flex",justifyContent:"center"}}>
                 <nav aria-label="Page navigation example">
                     <ul className="pagination">
-                        <li className="page-item"><a className="page-link" href="#">Previous</a></li>
+                        <li className="page-item"><a className="page-link" onClick={this.handlePrev}>Previous</a></li>
                         {this.state.pArr.map((ele)=>(
-                            <li className="page-item"><a className="page-link" href="#">{ele}</a></li>
+                            <li className="page-item"><a className="page-link" onClick={()=>this.handlePageClick(ele)}>{ele}</a></li>
                         ))}
                         
                         <li className="page-item"><a className="page-link" onClick={this.handleNext} >Next</a></li>
