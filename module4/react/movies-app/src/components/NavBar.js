6ddb85171321c2c2b54e8544ca8efa6ff8c2eccf
Shubham Gupta <mrjanuaryx@gmail.com>
2022-06-29 19:19:32 +0530

    update

--- module4/react/movies-app/src/components/NavBar.js
+++ module4/react/movies-app/src/components/NavBar.js
@@ -1,22 +1,16 @@
 import { Component } from "react";
-
+import { Link } from 'react-router-dom'
 
 class NavBar extends Component{
 
 
-    handleClick = ()=>{
-        document.querySelector(".container").classList.add("active");
-        document.querySelector(".movie-container").classList.add("block")
-        document.querySelector(".banner-card").classList.add("block")
-        
-
-        
-    }
+  
     render(){
         return(
         <div className="nav-bar"style={{display:"flex",padding:'0.5rem',color:"blue"}}>
-            <h1>Movies App</h1>
-            <h2 style={{marginLeft:"2rem",marginTop:"0.5rem"}}><button onClick={this.handleClick}>Favourites</button></h2>
+            <Link to="/"><h1>Movies App</h1></Link>   
+            <Link to="/favourites"><h2 style={{marginLeft:"2rem",marginTop:"0.5rem"}}>Favourites</h2></Link>
+            
         </div>
         )
     }
