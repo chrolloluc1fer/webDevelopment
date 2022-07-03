2e7adcf28bfd0300047d7669f6e2b9ee95164ef1
Shubham Gupta <mrjanuaryx@gmail.com>
2022-07-04 00:57:20 +0530

    update

---
+++ module4/react/functional-comp-react/src/components/Counter.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+
+function Counter(){
+    const[count,setCount] = useState(0);
+    const[text,setText] = useState('');
+    return(
+        <div>
+            <h1>This is counter {count}</h1>
+            <button type="" onClick={() => setCount(count + 1)}>+</button>
+            <button type="" onClick={() => setCount(count - 1)}>-</button>
+            <input   value={text} onChange = {(e) => setText(e.target.value)}></input>
+            <h1>{text}</h1>
+        </div>
+    )
+}
+
+export default Counter;
