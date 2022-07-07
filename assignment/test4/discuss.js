00a0383837c8bfa994400bfdd58d0aab6b331258
Shubham Gupta <mrjanuaryx@gmail.com>
2022-07-07 19:23:35 +0530

    udpate

--- assignment/test4/discuss.js
+++ assignment/test4/discuss.js
@@ -20,7 +20,7 @@
 //         yield i;
 //         i++;    
 //     }
-    
+
 // }
 
 // let num =  reachInfinite()
@@ -44,35 +44,132 @@
 // async function f1() {
 //     console.log(1);
 //   }
-  
+
 //   async function f1() {
 //     console.log(2);
 //   }
-  
+
 //   console.log(3);
 //   f1();
 //   console.log(1);
 
 
-let userObj = {
-    private:{
-        dob:"12/3/4"
-    },
-    public:{
-        name:"Anas"
-    }
-}
+// let userObj = {
+//     private:{
+//         dob:"12/3/4"
+//     },
+//     public:{
+//         name:"Anas"
+//     }
+// }
 
 
-let handler = {
-    get:function(target, prop){
-        if(prop == "private")
-        return  "Access not granted"
-        else
-        return target.prop
-    }
-}
+// let handler = {
+//     get:function(target, prop){
+//         if(prop == "private")
+//         return  "Access not granted"
+//         else
+//         return target.prop
+//     }
+// }
 
-let p =  new Proxy(userObj,handler)  // proxy object
+// let p =  new Proxy(userObj,handler)  // proxy object
 
-console.log(p.private)
+// console.log(p.private)
+
+
+// let obj = {
+//     a: {
+//         b: { e: { string: "string" } },
+//         c: { boolean: true },
+//     },
+//     d: {
+//         f: {
+//             g: {
+//                 h: { null: null },
+//                 i: { undefined: "defined" },
+//             },
+//         },  
+//     },
+// };
+
+// let {
+//     a: {
+//         b: {
+//             e: { string },
+//         },
+//     },
+// } = obj;
+
+// let {
+//     d: {
+//         f: {
+//             g: {
+//                 i: { undefined },
+//             },
+//         },
+//     },
+// } = obj;
+
+// console.log(`${string}-${undefined}`);
+
+// let a = {
+//     a:Symbol()
+    
+// }
+
+// a[ a] = 2
+// a[ Symbol()] = 3;
+
+// console.log(a[Symbol()]);
+
+
+// let a = Symbol()
+// console.log(a);
+
+// let b = Symbol("Shubham")
+// console.log(b.description)
+
+// let c = Symbol("Js")
+// console.log( b == c)
+// b == c
+
+
+
+
+// let a = [1, 2, 2, 3, 4, 5, 5];
+
+// let s = new Set([...a]);
+
+// console.log(s);
+
+// for (let i = 1; i < 5; i++) {
+//   s.delete(i);
+// }
+
+// console.log(s);
+
+// s.clear();
+
+// for (let i = 1; i < 5; i++) {
+//   s.add(i);
+// }
+
+// console.log(s);
+
+// for (let i = 1; i < 5; i++) {
+//   s.add(i);
+// }
+
+// console.log(s);
+
+
+
+let arr = [1,2,3,4,5]
+
+let ans =  arr.map((element,index)=>{
+   
+    return {index: element}
+})
+
+console.log(ans);
