4d9f675876cf0035e92e17cc4774c39b2055cce4
Shubham Gupta <mrjanuaryx@gmail.com>
2022-07-17 20:00:23 +0530

    update

--- module4/react/reels-app/src/components/Login.jsx
+++ module4/react/reels-app/src/components/Login.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { useState } from "react";
+import { auth } from "../Firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+  const [loader, setLoader] = useState(false);
+  const [error, setError] = useState("");
 
   const trackEmail = function (e) {
     setEmail(e.target.value);
@@ -13,20 +18,34 @@ function Login() {
     setPassword(e.target.value);
   };
 
-  const printDetails = function () {
-    alert(email + password)
+  const printDetails = async function () {
+    try {
+      setLoader(true);
+      let userCred = await signInWithEmailAndPassword(auth, email, password);
+      setUser(userCred.user);
+    } catch (err) {
+      setError(err.message);
+
+      setTimeout(() => {
+        setError("");
+      }, 3000);
+    }
+
+    setLoader(false);
   };
 
   return (
-    <div>
-      <input type="email" onChange={trackEmail} placeholder="email" />
+    <>{
+      error != "" ? <h1>Error is {error}</h1>:
+      loader === true ? <h1>...loading</h1>:
+      user != null ? <h1>User is {user.uid}</h1>:
+      <><input type="email" onChange={trackEmail} placeholder="email" />
       <br></br>
       <input type="password" onChange={trackPassword} placeholder="password" />
       <br></br>
-      <button type="click" onClick={printDetails}>
-        Login
-      </button>
-    </div>
+      <button type="click" onClick={printDetails}>Login</button></>
+      }
+    </>
   );
 }
 
