4a7cb247b646dde9d96523eba6c53cec9a7cb520
Shubham Gupta <mrjanuaryx@gmail.com>
2022-07-17 19:33:14 +0530

    login demo

--- module4/react/reels-app/src/components/Login.jsx
+++ module4/react/reels-app/src/components/Login.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const trackEmail = function (e) {
+    setEmail(e.target.value);
+  };
+
+  const trackPassword = function (e) {
+    setPassword(e.target.value);
+  };
+
+  const printDetails = function () {
+    alert(email + password)
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div>
+      <input type="email" onChange={trackEmail} placeholder="email" />
+      <br></br>
+      <input type="password" onChange={trackPassword} placeholder="password" />
+      <br></br>
+      <button type="click" onClick={printDetails}>
+        Login
+      </button>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
