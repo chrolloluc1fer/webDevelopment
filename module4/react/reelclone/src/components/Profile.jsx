50f12a1c3c17d3bfa0bd820264dc7b2e958b1b03
Shubham Gupta <mrjanuaryx@gmail.com>
2022-07-18 19:35:24 +0530

    update

--- module4/react/reelclone/src/components/Profile.jsx
+++ module4/react/reelclone/src/components/Profile.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import "./profile.css";
 function Profile() {
   return (
-    <div>Profile</div>
-  )
+    <>
+      <div className="header"></div>
+      <div className="main">
+        <div className="pimg_container">
+          <img src="http://via.placeholder.com/640x360" alt="" className="pimg"/>
+        </div>
+        <div className="details">
+          <div className="content">Shubham</div>
+          <div className="content">No of Posts: <span className="bold_text">Posts</span></div>
+          <div className="content">Email: <span className="bold_text">Email.com</span></div>
+         
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Profile
+export default Profile;
