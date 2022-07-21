cbb45cd9cedfb1c6b0b8042a218ef6a647f03bfc
Shubham Gupta <mrjanuaryx@gmail.com>
2022-07-21 20:43:55 +0530

    update

--- module4/react/reelclone/src/components/Profile.jsx
+++ module4/react/reelclone/src/components/Profile.jsx
@@ -1,31 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./profile.css";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { useState } from "react";
+import { db } from '../Firebase'
+import { doc, getDoc } from 'firebase/firestore'
 
 function Profile() {
   let cUser = useContext(AuthContext);
-  return (
+  let [loading, setLoading] = useState(true)
 
+  return (
     <>
     {
-      cUser == null ? <div>Need to Login</div>:<div>Logged in user is{cUser.uid}</div>
-          }
-    </>
-    // <>
-    //   <div className="header"></div>
-    //   <div className="main">
-    //     <div className="pimg_container">
-    //       <img src="http://via.placeholder.com/640x360" alt="" className="pimg"/>
-    //     </div>
-    //     <div className="details">
-    //       <div className="content">Shubham</div>
-    //       <div className="content">No of Posts: <span className="bold_text">Posts</span></div>
-    //       <div className="content">Email: <span className="bold_text">Email.com</span></div>
+      cUser == null ? <div>Need to Login</div>:
+          
+    <>
+    
+        <div className="header"></div>
+       <div className="main">
+         <div className="pimg_container">
+           <img src="http://via.placeholder.com/640x360" alt="" className="pimg"/>
+         </div>
+         <div className="details">
+           <div className="content">Shubham</div>
+           <div className="content">No of Posts: <span className="bold_text">Posts</span></div>
+           <div className="content">Email: <span className="bold_text">Email.com</span></div>
          
-    //     </div>
-    //   </div>
-    // </>
+         </div>
+       </div>
+    </>}
+    </>
   );
 }
 
