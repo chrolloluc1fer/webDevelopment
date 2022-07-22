d5aa240877cd9182d7027df986ca061dbe914e54
Shubham Gupta <mrjanuaryx@gmail.com>
2022-07-22 22:30:51 +0530

    feed styling done

--- module4/react/reelclone/src/components/VideoCard.jsx
+++ module4/react/reelclone/src/components/VideoCard.jsx
@@ -8,53 +8,50 @@ function VideoCard() {
 
   return (
     <div className="video-card">
-      <p className="video-card-username">Fake User</p>
-      <span className="video-card-music">
-        <span>music note</span>
-        <marquee behavior="" direction="">Song name</marquee>
-      </span>
+      <div className="left">
+        <p className="video-card-username">Shubham</p>
+        <span className="video-card-music">
+         <p behavior="" direction="">Song name</p>
+        </span>
+      </div>
       
-      <span
-        onClick={(e) => {
-          if (commentBoxOpen) {
-            setCommentBox(false);
-          } else {
-            setCommentBox(true);
-          }
-        }}
-      >
-        Chat
-      </span>
-
-      {commentBoxOpen ? (
-        <div className="video-card-comment-box">
-            <div className="actual-comment">
-            <h5>User Name</h5>
-          <p>This is actual Comment</p>
-            </div>
-         
-          <div className="comment-form">
-            <div className="post-user-comment">
-            <input type="text" />
-            <button>Post</button>
-            </div>
-           
-          </div>
+    
+      <div className="right">
+        <div className="videoContainer">
+          <video
+            onClick={(e) => {
+              if (playing) {
+                e.currentTarget.pause();
+                setPlaying(false);
+              } else {
+                e.currentTarget.play();
+                setPlaying(true);
+              }
+            }}
+            src="https://firebasestorage.googleapis.com/v0/b/class-demo-620a8.appspot.com/o/production%20ID_4434242.mp4?alt=media&token=3616c4ef-e3e0-428d-9274-32bdb6d4020a"
+          ></video>
         </div>
-      ) : ("")}
+        
+        <div className="comment">
+         <h3>Comments</h3> 
+          <div className="video-card-comment-box">
+              <div className="actual-comment">
+             <h4>User</h4>:
+              <p>This is actual an Comment</p>
+              </div>
+        
+            <div className="comment-form">
+              <div className="post-user-comment">
+              <input type="text" />
+              <button>Post</button>
+              </div>
+        
+            </div>
+          </div>
+         
+          </div>
+      </div>
 
-      <video
-        onClick={(e) => {
-          if (playing) {
-            e.currentTarget.pause();
-            setPlaying(false);
-          } else {
-            e.currentTarget.play();
-            setPlaying(true);
-          }
-        }}
-        src="https://firebasestorage.googleapis.com/v0/b/class-demo-620a8.appspot.com/o/production%20ID_4434242.mp4?alt=media&token=3616c4ef-e3e0-428d-9274-32bdb6d4020a"
-      ></video>
     </div>
   );
 }
