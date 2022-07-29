6ac5f94df68c333cd1fe217cfe1d5c25b869964b
Shubham Gupta <mrjanuaryx@gmail.com>
2022-07-29 17:15:36 +0530

    update

---
+++ module4/react/reelclone/src/secret.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBv3WgTmEwLuC15luf8oXpa90cq5_F-u9U",
+    authDomain: "class-demo-620a8.firebaseapp.com",
+    projectId: "class-demo-620a8",
+    storageBucket: "class-demo-620a8.appspot.com",
+    messagingSenderId: "775223827643",
+    appId: "1:775223827643:web:889e8e5f6bd8a6bebc9d9e"
+};
+
+export default firebaseConfig
