6d6d92cda51d9255c9e8d1864c883b901e05b812
Shubham Gupta <mrjanuaryx@gmail.com>
2022-07-30 16:45:39 +0530

    update

--- module4/shopping-cart/src/components/Preview.jsx
+++ module4/shopping-cart/src/components/Preview.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-
+import React from "react";
+import "./preview.css";
 function Preview() {
   return (
     <>
-    <div>
-        Preview
-    </div>
+      <div className="preview-container">
+        <div className="preview-img-container">
+          <img
+            src="https://m.media-amazon.com/images/I/71fJ-gmBZtL._SL1500_.jpg"
+            alt=""
+          />
+        </div>
+        <div className="preview-listing">
+          <h2>iPhone</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+            repellendus voluptates neque, a ipsa suscipit eos sunt non excepturi
+            quam.
+          </p>
+          <button className="add-to-cart">Add to Cart</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Preview
+export default Preview;
