eabaddb04f1f484e3da043da899387d57893c536
Shubham Gupta <mrjanuaryx@gmail.com>
2022-08-08 20:57:52 +0530

    update

--- module4/resume-builder/src/index.js
+++ module4/resume-builder/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <App />
-  </Router>
-    
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+
+
 );
 
---
+++ module4/resume-builder/src/redux/reducers/rootReducer.js
@@ -0,0 +1,10 @@
+import documentReducer from "./documentReducer";
+import { combineReducers } from "redux";
+
+
+const rootReducer = combineReducers({
+    documentReducer:documentReducer
+})
+
+
+export default rootReducer;
