39d60550d52b622992b83ee4d73a8f70dd97fe22
Shubham Gupta <mrjanuaryx@gmail.com>
2022-08-08 21:19:33 +0530

    update

--- module4/resume-builder/src/components/Contact.jsx
+++ module4/resume-builder/src/components/Contact.jsx
@@ -2,8 +2,26 @@ import React from "react";
 import Preview from "./Preview";
 import "./contact.css";
 import { Link } from "react-router-dom";
-
+import { useSelector } from "react-redux";
+import { useState } from "react";
 function Contact() {
+  const [contact, setContact] = useState({});
+
+  console.log(contact);
+  
+  const onChange = (event) => {
+    let key = event.target.id;
+    let value = event.target.value;
+    setContact({ ...contact, [key]: value });
+  };
+
+  const getFieldData = (key) => {
+    if(contact[key]){
+      return contact[key]
+    }else{
+      return "";
+    }
+  }
   return (
     <div className="contact">
       <div className="contact-form">
@@ -13,52 +31,53 @@ function Contact() {
         <div className="contact-form-details">
           <div className="input-group">
             <label htmlFor="">First Name</label>
-            <input type="text" id="fname" value="" />
+            <input type="text" id="fname" value={getFieldData("fname")} onChange={onChange} />
           </div>
           <div className="input-group">
             <label htmlFor="">Last Name</label>
-            <input type="text" id="lname" value="" />
+            <input type="text" id="lname" value={getFieldData("lname")} onChange={onChange} />
           </div>
           <div className="input-group full">
             <label htmlFor="">Professional Summary</label>
-            <input type="text" id="summary" value="" />
+            <input type="text" id="summary" value={getFieldData("summary")} onChange={onChange} />
           </div>
           <div className="input-group">
             <label htmlFor="">Email</label>
-            <input type="text" id="email" value="" />
+            <input type="text" id="email" value={getFieldData("email")} onChange={onChange} />
           </div>
           <div className="input-group">
             <label htmlFor="">Phone No</label>
-            <input type="text" id="phoneno" value="" />
+            <input type="text" id="phoneno" value={getFieldData("phoneno")} onChange={onChange} />
           </div>
           <div className="input-group">
             <label htmlFor="">Profession</label>
-            <input type="text" id="profession" value="" />
+            <input type="text" id="profession" value={getFieldData("profession")} onChange={onChange} />
           </div>
           <div className="input-group">
             <label htmlFor="">Street</label>
-            <input type="text" id="street" value="" />
+            <input type="text" id="street" value={getFieldData("street")} onChange={onChange} />
           </div>
           <div className="input-group">
             <label htmlFor="">City</label>
-            <input type="text" id="city" value="" />
+            <input type="text" id="city" value={getFieldData("city")} onChange={onChange} />
           </div>
           <div className="input-group">
             <label htmlFor="">State</label>
-            <input type="text" id="state" value="" />
+            <input type="text" id="state" value={getFieldData("state")} onChange={onChange} />
           </div>
           <div className="input-group">
             <label htmlFor="">Country</label>
-            <input type="text" id="country" value="" />
+            <input type="text" id="country" value={getFieldData("country")} onChange={onChange} />
           </div>
           <div className="input-group">
             <label htmlFor="">Pin Code</label>
-            <input type="text" id="pincode" value="" />
+            <input type="text" id="pincode" value={getFieldData("pincode")} onChange={onChange} />
           </div>
-        <Link to="/education"> <div className="next">
-            <button>Next</button>
-          </div></Link> 
-         
+          <Link to="/education">
+            <div className="next">
+              <button>Next</button>
+            </div>
+          </Link>
         </div>
       </div>
       <div className="preview-form">
--- module4/resume-builder/src/redux/reducers/documentReducer.js
+++ module4/resume-builder/src/redux/reducers/documentReducer.js
@@ -2,7 +2,7 @@ import initialState from './intialState.json'
 import * as actionTypes from "../action.js"
 
 
-export default function documentReducer(state = initialState.document, action) {
+export default function documentReducer(state = initialState, action) {
     switch(action.type){
         case actionTypes.SET_SKIN:
             return{
