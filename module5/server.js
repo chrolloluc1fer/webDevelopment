03ca1479e65779fb45605ef1a771a48975d782ae
Shubham Gupta <mrjanuaryx@gmail.com>
2022-08-13 20:03:20 +0530

    update

--- module5/server.js
+++ module5/server.js
@@ -5,8 +5,34 @@ const express = require("express")
 //you have to write it -> app signifies -> your server
 const app = express();
 
+app.use(express.json())
+
 app.get("/sayHello", function(req, res){
     res.send("Hello from Server")
+})
+
+app.get("/", function(req, res){
+    res.send("HomePage")
+})
+
+// app.post("/sayHello",(req, res)=>{
+//     console.log(req)
+//     res.json({
+//         message:"Data received successfully"
+//     })
+// })
+
+// app.patch("/sayHello",function(req, res){
+//     console.log(res)
+//     res.send("Patch wala hello")
+// })
+
+app.get("/getSquare/:num1/:num2",(req, res)=>{
+    console.log(req.params)
+    let num1 = req.params.num1;
+    let num2 = req.params.num2
+    let multiple = num1 * num2
+    res.send("Multiple is : " +multiple);
 })
 
 app.get("/sayBye", function(req, res){
