e98b3f419e52733999b4ace92cfd0c22505d1816
Shubham Gupta <mrjanuaryx@gmail.com>
2022-08-13 17:42:24 +0530

    update

--- module5/server.js
+++ module5/server.js
@@ -9,6 +9,10 @@ app.get("/sayHello", function(req, res){
     res.send("Hello from Server")
 })
 
+app.get("/sayBye", function(req, res){
+    res.send("Bye from server")
+})
+
 app.listen(3000,function(){
     console.log("Server started at port 3000")
 })
